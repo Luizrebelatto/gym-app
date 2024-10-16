1efa76ca91970f40325fb1db2f7225f8e52ca3bd
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2024-10-16 13:45:41 -0300

    fix: fix files styled and include paths tsconfig

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,38 +1,34 @@
-import "styled-components/native"
-
-declare module "styled-components/native" {
-    export interface DefaultTheme {
-        colors: {
-            primary: {
-                orange_dark: string;
-                orange: string;
-                orange_light: string
-            },
-            secondary: {
-                grey_dark: string;
-                grey: string;
-                pink_light: string;
-            },
-            auxiliary: {
-                red_dark: string;
-                red: string;
-                red_light: string;
-            },
-            gray: {
-                gray01: string;
-                gray02: string;
-                gray03: string;
-                gray04: string;
-                gray05: string;
-            },
-            gradient01: {
-                color01: string;
-                color02: string;
-            },
-            gradient02: {
-                color01: string;
-                color02: string;
-            }
+export default {
+    colors: {
+        primary: {
+            orange_dark: "#F65D09",
+            orange: "#FF7022",
+            orange_light: "#FF8F51"
+        },
+        secondary: {
+            grey_dark: "#C3C1C1",
+            grey: "#D8D8D8",
+            pink_light: "#F9F9F9"
+        },
+        auxiliary: {
+            red_dark: "#DE3961",
+            red: "#F64F77",
+            red_light: "#734DFF"
+        },
+        gray: {
+            gray01: "#34323E",
+            gray02: "#4E4B59",
+            gray03: "#5F5C6B",
+            gray04: "#7A7786",
+            gray05: "#8C8A97"
+        },
+        gradient01: {
+            color01: "#F65D09",
+            color02: "#FF8F51"
+        },
+        gradient02: {
+            color01: "#92A3FD",
+            color02: "#9DCEFF"
         }
     }
 }
